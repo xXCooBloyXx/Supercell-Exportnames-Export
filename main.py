cf011f3f6bc8f9ceabf9ca8c1d6ca307270e74d1
xXCooBloyXx <89714046+xXCooBloyXx@users.noreply.github.com>
2023-11-11 20:04:00 +0100

    Big update! works on all files

--- main.py
+++ main.py
@@ -1,47 +1,50 @@
 import os
-import re
+from sc_compression import decompress
+import struct
 
 input_dir = "./input"
 output_dir = "./output"
 
 if not os.path.exists(input_dir):
-    os.makedirs(input_dir)
-    quit()
+	os.makedirs(input_dir)
+	quit()
 if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-    quit()
+	os.makedirs(output_dir)
+	quit()
 
-def extract_export_names(data):
-    export_names = []
-    i = data.find(b"START") + 5
-    while i < len(data):
-        export_name = ""
-        j = i
-        while j < len(data) and data[j] != 0:
-            export_name += chr(data[j])
-            j += 1
-        if len(export_name) > 0:
-            export_names.append(export_name)
-        if j == len(data):
-            break
-        i = j + 18
-        if i < len(data):
-            next_char = chr(data[i])
-            if not re.match(r"[a-z0-9_]", next_char):
-                break
-    return export_names
-
+def dec(file):
+	file = file[:-3]
+	with open(f'./{file}.sc', 'rb') as f:
+		data = f.read()
+	if b"START" in data:
+		data = data.split(b"START")[0]
+	with open(f'./{file}_decoded.sc', 'wb') as f:
+		f.write(decompress(data)[0])
+		
 for filename in os.listdir(input_dir):
-    input_path = os.path.join(input_dir, filename)
-    with open(input_path, "rb") as input_file:
-        input_data = input_file.read()
-        export_names = extract_export_names(input_data)
-        output_filename = os.path.splitext(filename)[0] + ".txt"
-        output_path = os.path.join(output_dir, output_filename)
-        with open(output_path, "w", encoding="utf-8") as output_file:
-            for export_name in export_names:
-                output_file.write(export_name + "\n")
-            line_count = len(export_names)
-            print(f"Successfully exported {line_count} export names from {filename}!")
+	if filename.endswith(".sc"):
+		if not filename.endswith("_tex.sc"):
+			input_path = os.path.join(input_dir, filename)
+			dec(input_path)
+			decfilepath = os.path.join(input_dir, filename[:-3]+"_decoded.sc")
+			exportnames = []
+			with open(decfilepath, "rb") as file:
+				offset = 17
+				file.seek(offset)
+				exportCount = struct.unpack('<H', file.read(2))[0]
+				offset += exportCount * 2 + 2 
+				for i in range(exportCount):
+					file.seek(offset)
+					exportlength = struct.unpack('<B', file.read(1))[0]
+					offset += 1
+					file.seek(offset)
+					exportnames.append(file.read(exportlength).decode('utf-8'))
+					offset += exportlength
+				out = os.path.join(output_dir, os.path.basename(filename)[:-2]+"txt")
+				with open(out, "w", encoding="utf-8") as f:
+					for i in exportnames:
+						f.write(i+"\n")
+				print(f"Succesfully exported {exportCount} exportnames from {filename}")
+			os.remove(decfilepath)
 print("Done!")
 os.system("pause")
